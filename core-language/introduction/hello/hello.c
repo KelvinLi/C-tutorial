29ffe357fdf45da13e0316bdda7708f186d8b8d3
Kelvin Li <ltwisabc@gmail.com>
2013-02-08 13:46:03 -0800

    introduction: clarify Makefile usage
    "use the Makefile" is not explicit. Tell users how to build
    and clean projects.

--- core-language/introduction/hello/hello.c
+++ core-language/introduction/hello/hello.c
@@ -9,7 +9,11 @@
  * To run:
  *     ./a.out
  *
- * Alternatively, use the Makefile.
+ * Alternatively, use the Makefile:
+ *     make
+ *
+ * The Makefile can also clean up the build products:
+ *     make clean
  */
 
 #include <stdio.h>
