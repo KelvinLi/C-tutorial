cc1161e735d491af8d1f884f710d3b60fda852a6
Kelvin Li <ltwisabc@gmail.com>
2013-11-26 11:56:58 -0800

    arg-struct: simplify presentation

--- core-language/functions/arg-struct.c
+++ core-language/functions/arg-struct.c
@@ -1,59 +1,61 @@
 #include <stdio.h>
+#include <string.h>
 
 struct tomato {
-    int redness;
+    int spots;
     char name[10];
 };
 
-void
-display_status (struct tomato *t)
+int
+tomato_print (struct tomato *t)
 {
-    printf ("tomato{redness=(%d) name=(%s)}\n",
-            t->redness, t->name);
+    return printf("%s tomato with %d spots", t->name, t->spots);
 }
 
 void
 fry (struct tomato t)
 {
-    t.redness = t.redness / 2;
-    t.name[0] = 'x';
-    t.name[1] = 'y';
-    t.name[2] = 'z';
-    printf ("In fry(), I got: ");
-    display_status (&t);
+    const char fried_name[] = "FRIED";
+
+    t.spots = t.spots + 100;
+    memcpy (t.name, fried_name, sizeof (fried_name) / sizeof (*fried_name));
+    fputs ("mutated by fry(): ", stdout);
+    tomato_print (&t);
+    fputs ("\n", stdout);
 }
 
 void
 boil (struct tomato *t)
 {
-    char newname[10] = "boiled";
-    t->redness = t->redness / 2;
-    t->name[0] = 'x';
-    t->name[1] = 'y';
-    t->name[2] = 'z';
-    printf ("In boil(), I got: ");
-    display_status (t);
+    const char boiled_name[] = "BOILED";
+
+    t->spots = t->spots + 10;
+    memcpy (t->name,
+            boiled_name,
+            sizeof (boiled_name) / sizeof (*boiled_name));
+    fputs ("mutated by boil(): ", stdout);
+    tomato_print (t);
+    fputs ("\n", stdout);
 }
 
 int
 main (int argc, char **argv)
 {
-    struct tomato thom = {112, "Thomas"};
+    struct tomato thom = {2, "RAW"};
 
-    printf ("Initial: ");
-    display_status (&thom);
-    printf ("\n");
+    fputs ("I have a ", stdout);
+    tomato_print (&thom);
+    fputs (".\n\n", stdout);
 
     fry (thom);
-
-    printf ("After fry(): ");
-    display_status (&thom);
-    printf ("\n");
+    fputs ("I still have a ", stdout);
+    tomato_print (&thom);
+    fputs (".\n\n", stdout);
 
     boil (&thom);
-
-    printf ("After boil(): ");
-    display_status (&thom);
+    fputs ("Now I have a ", stdout);
+    tomato_print (&thom);
+    fputs (".\n", stdout);
 
     return 0;
 }
